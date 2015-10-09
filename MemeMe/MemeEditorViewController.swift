e66c72ec718c9b44bdae05d6899fb3fc7555cfa5
gaw3 <gwhite2003@verizon.net>
2015-10-09 01:15:50 -0500

    Added top & bottom text fields w/ desired behavior

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -8,17 +8,20 @@
 
 import UIKit
 
-class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegate,
+										  UINavigationControllerDelegate, UITextFieldDelegate {
 
 	// MARK: - IB Outlets
 
-	@IBOutlet weak var cancelButton: UIBarButtonItem!
 	@IBOutlet weak var actionButton: UIBarButtonItem!
-
-	@IBOutlet weak var photosButton: UIBarButtonItem!
 	@IBOutlet weak var cameraButton: UIBarButtonItem!
+	@IBOutlet weak var cancelButton: UIBarButtonItem!
+	@IBOutlet weak var photosButton: UIBarButtonItem!
 
 	@IBOutlet weak var pickedImageView: UIImageView!
+
+	@IBOutlet weak var topMemeTextField: UITextField!
+	@IBOutlet weak var bottomMemeTextField: UITextField!
 
 	// MARK: - Class Variables
 
@@ -26,6 +29,18 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+
+		let memeTextAttributes = [
+			NSStrokeColorAttributeName : UIColor.blackColor(),
+			NSForegroundColorAttributeName : UIColor.whiteColor(),
+			NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+			NSStrokeWidthAttributeName : -3.0
+		]
+
+		topMemeTextField.defaultTextAttributes = memeTextAttributes
+		topMemeTextField.textAlignment = .Center
+		bottomMemeTextField.defaultTextAttributes	= memeTextAttributes
+		bottomMemeTextField.textAlignment = .Center
 	}
 
 	override func viewWillAppear(animated: Bool) {
@@ -36,40 +51,69 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
 
 		cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
 		photosButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary)
+
+		subscribeToKeyboardNotifications()
+	}
+
+	override func viewWillDisappear(animated: Bool) {
+		super.viewWillDisappear(animated)
+
+		unsubscribeFromKeyboardNotifications()
 	}
 
 	// MARK: - IB Actions
 
 	@IBAction func actionButtonWasClicked(sender: UIBarButtonItem) {
+
 		if sender == actionButton {
 			// TODO: implement activity view
-		} else {
+		}
+		else {
 			// TODO: how to handle this?
 		}
 	}
 
 	@IBAction func cameraButtonWasClicked(sender: UIBarButtonItem) {
+
 		if sender == cameraButton {
 			pickImageFromSource(UIImagePickerControllerSourceType.Camera)
-		} else {
+		}
+		else {
 			// TODO: how to handle this?
 		}
+
 	}
 
 	@IBAction func cancelButtonWasClicked(sender: UIBarButtonItem) {
+
 		if sender == cancelButton {
 			// TODO: how to handle this?
-		} else {
+		}
+		else {
 			// TODO: how to handle this?
 		}
+
 	}
 
 	@IBAction func photosButtonWasClicked(sender: UIBarButtonItem) {
+
 		if sender == photosButton {
 			pickImageFromSource(UIImagePickerControllerSourceType.PhotoLibrary)
-		} else {
+		}
+		else {
 			// TODO: how to handle this?
 		}
+
+	}
+
+	// MARK: - NSNotifications
+
+	func keyboardWillHide(notification: NSNotification) {
+		self.view.frame.origin.y += getKeyboardHeight(notification)
+	}
+
+	func keyboardWillShow(notification: NSNotification) {
+		self.view.frame.origin.y -= getKeyboardHeight(notification)
 	}
 
 	// MARK: - UIImagePickerControllerDelegate
@@ -87,6 +131,55 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
 		dismissViewControllerAnimated(true, completion: nil)
 	}
 
+	// MARK: - UITextFieldDelegate
+
+	func textFieldShouldReturn(textField: UITextField) -> Bool {
+		var doDefault: Bool = true
+
+		textField.resignFirstResponder()
+
+		if textField.text == "" {
+			doDefault = false
+
+			if textField == topMemeTextField {
+				textField.text = "TOP"
+			}
+			else if textField == bottomMemeTextField {
+				textField.text = "BOTTOM"
+			}
+			else {
+				print("unrecognized text field")
+			}
+
+		}
+
+      return doDefault
+	}
+
+	// MARK: - Private:  Keyboards
+
+	private func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+		var keyboardHeight: CGFloat = 0.0
+
+		if bottomMemeTextField.isFirstResponder() {
+			let keyboardSize = notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+
+			keyboardHeight = keyboardSize.CGRectValue().height
+		}
+
+		return keyboardHeight
+	}
+
+	private func subscribeToKeyboardNotifications() {
+		NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+		NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+	}
+
+	private func unsubscribeFromKeyboardNotifications() {
+		NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+		NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+	}
+	
 	// MARK: - Private
 
 	private func pickImageFromSource(sourceType: UIImagePickerControllerSourceType) {
